f1e8aa4ff239fa34cf2b0b58e8f785bc286b603c
Vyshnav.K <vyshnavk117@gmail.com>
2024-02-22 15:13:52 +0530

    Refactor EditTopicForm component

--- src/components/EditTopicForm.jsx
+++ src/components/EditTopicForm.jsx
@@ -1,24 +1,22 @@
 const EditTopicForm = () => {
-    return (
-      <form  className="flex flex-col gap-3 mt-5">
+  return (
+    <form className='flex flex-col gap-3 mt-5'>
       <input
-       
-        className="border border-slate-500 px-8 py-2"
-        type="text"
-        placeholder="Topic Title"
+        className='border border-slate-500 px-8 py-2'
+        type='text'
+        placeholder='Topic Title'
       />
-  
+
       <input
-       
-        className="border border-slate-500 px-8 py-2"
-        type="text"
-        placeholder="Topic Description"
+        className='border border-slate-500 px-8 py-2'
+        type='text'
+        placeholder='Topic Description'
       />
-  
-      <button className="bg-green-600 font-bold text-white py-3 px-6 w-fit">
+
+      <button className='bg-green-600 font-bold text-white py-3 px-6 w-fit'>
         Update Topic
       </button>
     </form>
-    )
-  }
-  export default EditTopicForm
+  );
+};
+export default EditTopicForm;
