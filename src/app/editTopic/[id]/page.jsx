69f8ee23a95668ce3414a1641fb8885fcafcb88d
Vyshnav.K <vyshnavk117@gmail.com>
2024-02-22 17:42:35 +0530

    Refactor EditTopic page and EditTopicForm component

--- src/app/editTopic/[id]/page.jsx
+++ src/app/editTopic/[id]/page.jsx
@@ -1,9 +1,26 @@
-import EditTopicForm from "@/components/EditTopicForm";
+import EditTopicForm from '@/components/EditTopicForm';
 
-export default function EditTopic() {
-  return (
-    <div>
-      <EditTopicForm />
-    </div>
-  )
+const getTopicById = async (id) => {
+  try {
+    const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL;
+
+    const res = await fetch(`${baseURL}/api/topics/${id}`, {
+      cache: 'no-store',
+    });
+
+    if (!res.ok) {
+      throw new Error('Failed to fetch topic');
+    }
+
+    return res.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default async function EditTopic({ params }) {
+  const { id } = params;
+  const { topic } = await getTopicById(id);
+  const { title, description } = topic;
+  return <EditTopicForm id={id} title={title} description={description} />;
 }
--- src/components/EditTopicForm.jsx
+++ src/components/EditTopicForm.jsx
@@ -1,9 +1,13 @@
-"use client";
-import { useState } from "react";
-import { useRouter } from "next/navigation";
+'use client';
 
-const EditTopicForm = () => {
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { Button } from '@/components/ui/button';
+import { Card, CardHeader, CardTitle } from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Textarea } from '@/components/ui/textarea';
 
+export default function EditTopicForm({ id, title, description }) {
   const [newTitle, setNewTitle] = useState(title);
   const [newDescription, setNewDescription] = useState(description);
 
@@ -13,47 +17,50 @@ const EditTopicForm = () => {
     e.preventDefault();
 
     try {
-      const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL.id}`, {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/topics/${id}`, {
         method: "PUT",
         headers: {
           "Content-type": "application/json",
         },
         body: JSON.stringify({ newTitle, newDescription }),
       });
-
+    
       if (!res.ok) {
         throw new Error("Failed to update topic");
       }
-
+    
       router.refresh();
       router.push("/");
     } catch (error) {
       console.log(error);
     }
-  };
+  }    
+
 
   return (
-    <form onSubmit={handleSubmit} className='flex flex-col gap-3 mt-5'>
-      <input
-       onChange={(e) => setNewTitle(e.target.value)}
-       value={newTitle}
-        className='border border-slate-500 px-8 py-2'
-        type='text'
-        placeholder='Topic Title'
-      />
-
-      <input
-       onChange={(e) => setNewDescription(e.target.value)}
-       value={newDescription}
-        className='border border-slate-500 px-8 py-2'
-        type='text'
-        placeholder='Topic Description'
-      />
-
-      <button className='bg-green-600 font-bold text-white py-3 px-6 w-fit'>
-        Update Topic
-      </button>
-    </form>
+    <Card className='my-4 '>
+      <CardHeader>
+        <CardTitle>Add Topic</CardTitle>
+      </CardHeader>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-5 m-5'>
+        <Input
+          onChange={(e) => setNewTitle(e.target.value)}
+          value={newTitle}
+          className='border border-slate-500  py-4'
+          type='text'
+          placeholder='Topic Title'
+        />
+        <Textarea
+            onChange={(e) => setNewDescription(e.target.value)}
+            value={newDescription}
+          className='border border-slate-500  py-2'
+          type='text'
+          placeholder='Topic Description'
+        />
+        <Button type='submit' className='  py-3  w-full '>
+          Update Topic
+        </Button>
+      </form>
+    </Card>
   );
-};
-export default EditTopicForm;
+}
